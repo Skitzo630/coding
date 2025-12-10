e39834e67bc446a52a413e2637376f658e9d9e04
Skitzo630 <eliasthesavage800@gmail.com>
2025-12-10 13:54:35 +0000

    work

---
+++ castingactvity.py
@@ -0,0 +1,23 @@
+#str () changes any data type into a string 
+ #int() changes any data type into a integer (whole number) 
+#float() changes any data type into a float ( decimal number)
+ 
+  #probelm 1
+   # Name = input ("what is your name:")
+#Age = input ('what is your age:')
+ 
+#print ('hello my name is '+ Name + 'I am'+ Age + "Year old.")  
+ # string concatenation - combiing 2 or strings 
+
+ #problem 2 
+
+Bonus = "200" 
+#print (300 + int Bonus) # result should be 500
+ 
+#problem 3
+ #Hint: use w3schools to find a data casting function that will change 
+  # a data type into a boolean 
+   
+vaule = 'True'
+
+print ( True== bool (vaule) ) # result should be true 
